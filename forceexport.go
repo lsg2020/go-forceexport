d9e65dc443f8812d6fc767c83193e67d211c2ba8
珞水的大叔 <oncwnuDvPJwT7sSCM-vsrdAyaQ8g@git.weixin.qq.com>
2021-04-22 15:39:47 +0800

    add support for go1.16

--- forceexport.go
+++ forceexport.go
@@ -1,31 +1,34 @@
 package forceexport
 
 import (
-	"fmt"
-	"reflect"
+	go114 "github.com/AlaxLee/go-forceexport/go114"
+	go116 "github.com/AlaxLee/go-forceexport/go116"
+	"regexp"
 	"runtime"
-	"unsafe"
 )
+
+var GoVersion string
+
+func init() {
+	reg := regexp.MustCompile(`go\d+\.\d+`)
+	GoVersion = reg.FindString(runtime.Version())
+}
 
 // GetFunc gets the function defined by the given fully-qualified name. The
 // outFuncPtr parameter should be a pointer to a function with the appropriate
 // type (e.g. the address of a local variable), and is set to a new function
 // value that calls the specified function. If the specified function does not
 // exist, outFuncPtr is not set and an error is returned.
-func GetFunc(outFuncPtr interface{}, name string) error {
-	codePtr, err := FindFuncWithName(name)
-	if err != nil {
-		return err
+func GetFunc(outFuncPtr interface{}, name string) (err error) {
+	switch GoVersion {
+	case "go1.14", "go1.15":
+		err = go114.GetFunc(outFuncPtr, name)
+	case "go1.16":
+		err = go116.GetFunc(outFuncPtr, name)
+	default:
+		panic("Not suitable for " + GoVersion)
 	}
-	CreateFuncForCodePtr(outFuncPtr, codePtr)
-	return nil
-}
-
-// Convenience struct for modifying the underlying code pointer of a function
-// value. The actual struct has other values, but always starts with a code
-// pointer.
-type Func struct {
-	codePtr uintptr
+	return
 }
 
 // CreateFuncForCodePtr is given a code pointer and creates a function value
@@ -33,21 +36,14 @@ type Func struct {
 // of the proper type (e.g. the address of a local variable), and will be set to
 // the result function value.
 func CreateFuncForCodePtr(outFuncPtr interface{}, codePtr uintptr) {
-	outFuncVal := reflect.ValueOf(outFuncPtr).Elem()
-	// Use reflect.MakeFunc to create a well-formed function value that's
-	// guaranteed to be of the right type and guaranteed to be on the heap
-	// (so that we can modify it). We give a nil delegate function because
-	// it will never actually be called.
-	newFuncVal := reflect.MakeFunc(outFuncVal.Type(), nil)
-	// Use reflection on the reflect.Value (yep!) to grab the underling
-	// function value pointer. Trying to call newFuncVal.Pointer() wouldn't
-	// work because it gives the code pointer rather than the function value
-	// pointer. The function value is a struct that starts with its code
-	// pointer, so we can swap out the code pointer with our desired value.
-	funcValuePtr := reflect.ValueOf(newFuncVal).FieldByName("ptr").Pointer()
-	funcPtr := (*Func)(unsafe.Pointer(funcValuePtr))
-	funcPtr.codePtr = codePtr
-	outFuncVal.Set(newFuncVal)
+	switch GoVersion {
+	case "go1.14", "go1.15":
+		go114.CreateFuncForCodePtr(outFuncPtr, codePtr)
+	case "go1.16":
+		go116.CreateFuncForCodePtr(outFuncPtr, codePtr)
+	default:
+		panic("Not suitable for " + GoVersion)
+	}
 }
 
 // FindFuncWithName searches through the moduledata table created by the linker
@@ -55,148 +51,14 @@ func CreateFuncForCodePtr(outFuncPtr interface{}, codePtr uintptr) {
 // returns an error. Since the data structures here are not exported, we copy
 // them below (and they need to stay in sync or else things will fail
 // catastrophically).
-func FindFuncWithName(name string) (uintptr, error) {
-	for moduleData := &Firstmoduledata; moduleData != nil; moduleData = moduleData.next {
-		for _, ftab := range moduleData.ftab {
-			f := (*runtime.Func)(unsafe.Pointer(&moduleData.pclntable[ftab.funcoff]))
-			funcName, err := getFuncName(f)
-			if err == nil && funcName == name {
-				return f.Entry(), nil
-			}
-		}
+func FindFuncWithName(name string) (p uintptr, err error) {
+	switch GoVersion {
+	case "go1.14", "go1.15":
+		p, err = go114.FindFuncWithName(name)
+	case "go1.16":
+		p, err = go116.FindFuncWithName(name)
+	default:
+		panic("Not suitable for " + GoVersion)
 	}
-	return 0, fmt.Errorf("Invalid function name: %s", name)
-}
-
-// Everything below is taken from the runtime package, and must stay in sync
-// with it.
-
-//go:linkname Firstmoduledata runtime.firstmoduledata
-var Firstmoduledata Moduledata
-
-type Moduledata struct {
-	pclntable    []byte //include func (which type is runtime.Func) by funcoff and funcName (which type is string) by nameoff
-	ftab         []functab
-	filetab      []uint32
-	findfunctab  uintptr
-	minpc, maxpc uintptr
-
-	text, etext           uintptr
-	noptrdata, enoptrdata uintptr
-	data, edata           uintptr
-	bss, ebss             uintptr
-	noptrbss, enoptrbss   uintptr
-	end, gcdata, gcbss    uintptr
-	types, etypes         uintptr
-
-	textsectmap []textsect
-	typelinks   []int32 // offsets from types
-	itablinks   []*itab
-
-	ptab []ptabEntry
-
-	pluginpath string
-	pkghashes  []modulehash
-
-	modulename   string
-	modulehashes []modulehash
-
-	hasmain uint8 // 1 if module contains the main function, 0 otherwise
-
-	gcdatamask, gcbssmask bitvector
-
-	typemap map[typeOff]*_type // offset to *_rtype in previous module
-
-	bad bool // module failed to load and should be ignored
-
-	next *Moduledata
-}
-
-type functab struct {
-	entry   uintptr
-	funcoff uintptr
-}
-
-type textsect struct {
-	vaddr    uintptr // prelinked section vaddr
-	length   uintptr // section length
-	baseaddr uintptr // relocated section address
-}
-
-type itab struct {
-	inter *interfacetype
-	_type *_type
-	hash  uint32 // copy of _type.hash. Used for type switches.
-	_     [4]byte
-	fun   [1]uintptr // variable sized. fun[0]==0 means _type does not implement inter.
-}
-
-type interfacetype struct {
-	typ     _type
-	pkgpath name
-	mhdr    []imethod
-}
-
-type _type struct {
-	size       uintptr
-	ptrdata    uintptr // size of memory prefix holding all pointers
-	hash       uint32
-	tflag      tflag
-	align      uint8
-	fieldAlign uint8
-	kind       uint8
-	// function for comparing objects of this type
-	// (ptr to object A, ptr to object B) -> ==?
-	equal func(unsafe.Pointer, unsafe.Pointer) bool
-	// gcdata stores the GC type data for the garbage collector.
-	// If the KindGCProg bit is set in kind, gcdata is a GC program.
-	// Otherwise it is a ptrmask bitmap. See mbitmap.go for details.
-	gcdata    *byte
-	str       nameOff
-	ptrToThis typeOff
-}
-
-type name struct {
-	bytes *byte
-}
-
-type imethod struct {
-	name nameOff
-	ityp typeOff
-}
-
-type tflag uint8
-type nameOff int32
-type typeOff int32
-
-type ptabEntry struct {
-	name nameOff
-	typ  typeOff
-}
-
-type modulehash struct {
-	modulename   string
-	linktimehash string
-	runtimehash  *string
-}
-
-type bitvector struct {
-	n        int32 // # of bits
-	bytedata *uint8
-}
-
-func getFuncName(f *runtime.Func) (funcName string, err error) {
-	// f.Name() may panic because runtime.findmoduledatap may return nil
-	defer func() {
-		if r := recover(); r != nil {
-			switch e := r.(type) {
-			case error:
-				err = e
-			default:
-				panic("unexpect")
-			}
-		}
-	}()
-	funcName = f.Name()
-	return funcName, err
+	return
 }
---
+++ go116/forceexport.go
@@ -0,0 +1,226 @@
+// for go1.16
+
+package forceexport
+
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+	"unsafe"
+)
+
+// GetFunc gets the function defined by the given fully-qualified name. The
+// outFuncPtr parameter should be a pointer to a function with the appropriate
+// type (e.g. the address of a local variable), and is set to a new function
+// value that calls the specified function. If the specified function does not
+// exist, outFuncPtr is not set and an error is returned.
+func GetFunc(outFuncPtr interface{}, name string) error {
+	codePtr, err := FindFuncWithName(name)
+	if err != nil {
+		return err
+	}
+	CreateFuncForCodePtr(outFuncPtr, codePtr)
+	return nil
+}
+
+// Convenience struct for modifying the underlying code pointer of a function
+// value. The actual struct has other values, but always starts with a code
+// pointer.
+type Func struct {
+	codePtr uintptr
+}
+
+// CreateFuncForCodePtr is given a code pointer and creates a function value
+// that uses that pointer. The outFun argument should be a pointer to a function
+// of the proper type (e.g. the address of a local variable), and will be set to
+// the result function value.
+func CreateFuncForCodePtr(outFuncPtr interface{}, codePtr uintptr) {
+	outFuncVal := reflect.ValueOf(outFuncPtr).Elem()
+	// Use reflect.MakeFunc to create a well-formed function value that's
+	// guaranteed to be of the right type and guaranteed to be on the heap
+	// (so that we can modify it). We give a nil delegate function because
+	// it will never actually be called.
+	newFuncVal := reflect.MakeFunc(outFuncVal.Type(), nil)
+	// Use reflection on the reflect.Value (yep!) to grab the underling
+	// function value pointer. Trying to call newFuncVal.Pointer() wouldn't
+	// work because it gives the code pointer rather than the function value
+	// pointer. The function value is a struct that starts with its code
+	// pointer, so we can swap out the code pointer with our desired value.
+	funcValuePtr := reflect.ValueOf(newFuncVal).FieldByName("ptr").Pointer()
+	funcPtr := (*Func)(unsafe.Pointer(funcValuePtr))
+	funcPtr.codePtr = codePtr
+	outFuncVal.Set(newFuncVal)
+}
+
+// FindFuncWithName searches through the moduledata table created by the linker
+// and returns the function's code pointer. If the function was not found, it
+// returns an error. Since the data structures here are not exported, we copy
+// them below (and they need to stay in sync or else things will fail
+// catastrophically).
+func FindFuncWithName(name string) (uintptr, error) {
+	for moduleData := &Firstmoduledata; moduleData != nil; moduleData = moduleData.next {
+		for _, ftab := range moduleData.ftab {
+			if int(ftab.funcoff) >= len(moduleData.pclntable) {
+				continue
+			}
+			f := (*runtime.Func)(unsafe.Pointer(&moduleData.pclntable[ftab.funcoff]))
+			funcName, err := getFuncName(f)
+			if err == nil && funcName == name {
+				return f.Entry(), nil
+			}
+		}
+	}
+	return 0, fmt.Errorf("Invalid function name: %s", name)
+}
+
+// Everything below is taken from the runtime package, and must stay in sync
+// with it.
+
+//go:linkname Firstmoduledata runtime.firstmoduledata
+var Firstmoduledata Moduledata
+
+type Moduledata struct {
+	pcHeader     *pcHeader
+	funcnametab  []byte
+	cutab        []uint32
+	filetab      []byte
+	pctab        []byte
+	pclntable    []byte
+	ftab         []functab
+	findfunctab  uintptr
+	minpc, maxpc uintptr
+
+	text, etext           uintptr
+	noptrdata, enoptrdata uintptr
+	data, edata           uintptr
+	bss, ebss             uintptr
+	noptrbss, enoptrbss   uintptr
+	end, gcdata, gcbss    uintptr
+	types, etypes         uintptr
+
+	textsectmap []textsect
+	typelinks   []int32 // offsets from types
+	itablinks   []*itab
+
+	ptab []ptabEntry
+
+	pluginpath string
+	pkghashes  []modulehash
+
+	modulename   string
+	modulehashes []modulehash
+
+	hasmain uint8 // 1 if module contains the main function, 0 otherwise
+
+	gcdatamask, gcbssmask bitvector
+
+	typemap map[typeOff]*_type // offset to *_rtype in previous module
+
+	bad bool // module failed to load and should be ignored
+
+	next *Moduledata
+}
+
+// pcHeader holds data used by the pclntab lookups.
+type pcHeader struct {
+	magic          uint32  // 0xFFFFFFFA
+	pad1, pad2     uint8   // 0,0
+	minLC          uint8   // min instruction size
+	ptrSize        uint8   // size of a ptr in bytes
+	nfunc          int     // number of functions in the module
+	nfiles         uint    // number of entries in the file tab.
+	funcnameOffset uintptr // offset to the funcnametab variable from pcHeader
+	cuOffset       uintptr // offset to the cutab variable from pcHeader
+	filetabOffset  uintptr // offset to the filetab variable from pcHeader
+	pctabOffset    uintptr // offset to the pctab varible from pcHeader
+	pclnOffset     uintptr // offset to the pclntab variable from pcHeader
+}
+
+type functab struct {
+	entry   uintptr
+	funcoff uintptr
+}
+
+type textsect struct {
+	vaddr    uintptr // prelinked section vaddr
+	length   uintptr // section length
+	baseaddr uintptr // relocated section address
+}
+
+type itab struct {
+	inter *interfacetype
+	_type *_type
+	hash  uint32 // copy of _type.hash. Used for type switches.
+	_     [4]byte
+	fun   [1]uintptr // variable sized. fun[0]==0 means _type does not implement inter.
+}
+
+type interfacetype struct {
+	typ     _type
+	pkgpath name
+	mhdr    []imethod
+}
+
+type _type struct {
+	size       uintptr
+	ptrdata    uintptr // size of memory prefix holding all pointers
+	hash       uint32
+	tflag      tflag
+	align      uint8
+	fieldAlign uint8
+	kind       uint8
+	// function for comparing objects of this type
+	// (ptr to object A, ptr to object B) -> ==?
+	equal func(unsafe.Pointer, unsafe.Pointer) bool
+	// gcdata stores the GC type data for the garbage collector.
+	// If the KindGCProg bit is set in kind, gcdata is a GC program.
+	// Otherwise it is a ptrmask bitmap. See mbitmap.go for details.
+	gcdata    *byte
+	str       nameOff
+	ptrToThis typeOff
+}
+
+type name struct {
+	bytes *byte
+}
+
+type imethod struct {
+	name nameOff
+	ityp typeOff
+}
+
+type tflag uint8
+type nameOff int32
+type typeOff int32
+
+type ptabEntry struct {
+	name nameOff
+	typ  typeOff
+}
+
+type modulehash struct {
+	modulename   string
+	linktimehash string
+	runtimehash  *string
+}
+
+type bitvector struct {
+	n        int32 // # of bits
+	bytedata *uint8
+}
+
+func getFuncName(f *runtime.Func) (funcName string, err error) {
+	// f.Name() may panic because runtime.findmoduledatap may return nil
+	defer func() {
+		if r := recover(); r != nil {
+			switch e := r.(type) {
+			case error:
+				err = e
+			default:
+				panic("unexpect")
+			}
+		}
+	}()
+	funcName = f.Name()
+	return funcName, err
+}
